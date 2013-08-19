c683a9a2cd7a0fb42966d038151c2417e4349982
Thomas Levine <occurrence@thomaslevine.com>
2013-08-19 10:58:23 -0700

    delete skipped tests that have already been rewritten

--- blogofile/tests/test_build.py
+++ blogofile/tests/test_build.py
@@ -68,33 +68,6 @@ class TestBuild(unittest.TestCase):
             os.path.join(self.build_path, "_site", "blog",
                          "2009", "07", "24", "post-2"))
 
-    def testNoPosts(self):
-        """Test when there are no posts, site still builds cleanly"""
-        main.main("init blog_unit_test")
-        main.config.override_options = {
-            "site_url": "http://www.test.com/",
-            "blog_path": "/blog"}
-        shutil.rmtree("_posts")
-        util.mkdir("_posts")
-        main.main("build")
-
-    def testPostInSubdir(self):
-        "Test a post in a subdirectory of _posts"
-        pass
-
-    def testNoPostsDir(self):
-        """Test when there is no _posts dir, site still builds cleanly"""
-        main.main("init blog_unit_test")
-        main.config.override_options = {
-            "site.url": "http://www.test.com/",
-            "blog.path": "/blog"}
-        shutil.rmtree("_posts")
-        logger = logging.getLogger("blogofile")
-        #We don't need to see the error that this test checks for:
-        logger.setLevel(logging.CRITICAL)
-        main.main("build")
-        logger.setLevel(logging.ERROR)
-
     def testCategoryPages(self):
         """Test that categories are written"""
         main.main("init blog_unit_test")
